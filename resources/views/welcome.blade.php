7cbe803f991da615d7806775f4347c1a04991dfd
unknown <luka.bulatovic.w@gmail.com>
2018-01-22 23:34:04 +0100

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Test GIT
                 </div>
 
                 <div class="links">
