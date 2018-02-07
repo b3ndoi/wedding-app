45bac151e40ba0224cd7dd3c6acbd93e3a19a136
unknown <luka.bulatovic.w@gmail.com>
2018-02-07 20:55:55 +0100

    Stripe dodatak

--- config/services.php
+++ config/services.php
@@ -31,8 +31,8 @@ return [
 
     'stripe' => [
         'model' => App\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
+        'key' => env('pk_test_xsQK6jHJPBGAy8WsUafghdyN'),
+        'secret' => env('sk_test_wdwPVHQjdIEWoUF1syDwoQV2'),
     ],
 
 ];
